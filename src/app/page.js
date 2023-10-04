bea77ed41c516b788e8ee441dd4bda2f956e7175
arberatD <arberatdushi@gmail.com>
2023-10-04 20:34:54 +0200

    weatherapp part 2

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,119 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import { useState, useEffect } from "react"
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+// ersten Buchstaben der Stadt groß schreiben
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+export default function Weatherapp() {
+    const [myCityName, setMyCityName] = useState("")
+  
+    // query wenn wir was auslesen
+    // mutation ist wenn wir unser state anpassen
+    const [ myCity, setMyCity ] = useState([])
+    
+    const constants = {
+        openWeatherMap: {
+            BASE_URL: 'https://api.openweathermap.org/data/2.5/weather?q=',
+            SECRET_KEY: 'bd0a4ee3d928010babd0d4abb9b6e88f' // enter your api key here
+        }
+    }
+
+    const weatherData = (address) => {
+        const url = constants.openWeatherMap.BASE_URL 
+                    + encodeURIComponent(address) 
+                    + '&lang=de&appid=' 
+                    + constants.openWeatherMap.SECRET_KEY;
+        return url;
+    };
+
+    const handleAddCity = () => {
+        const url = weatherData(myCityName);
+        fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Fehler beim Abruf');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const weatherDescription = data.weather[0].description;
+            const temperature = data.main.temp;
+            const humidity = data.main.humidity;
+            const capitalizedCityName = capitalizeFirstLetter(myCityName);
+            setMyCity([...myCity, { 
+                id: myCity.length + 1, 
+                description: `${capitalizedCityName} - ${weatherDescription}`,
+                temp: temperature,
+                humidity: humidity
+         }]);
+            setMyCityName("");
+        })
+        .catch(error => {
+            console.log("Es gab ein Problem mit der Fetch-Operation:", error.message);
+        });
+    };
+
+  
+    let testCss = {
+      padding: "50px"
+    }
+  
+    // 👇 this is an event handler
+    const handleCityInput = (event) => {
+      const value = event.target.value
+      setMyCityName(value)
+    }
+
+return (
+    <div>
+      <main>
+        <h1 style={testCss}>Abi´s NextJS Weather</h1>
+        <br />
+        <p>Rufe hier das Wetter ab</p>
+        <br />
+        <input 
+        value={myCityName}
+        onInput={handleCityInput}
+        style={{
+          padding: "1rem",
+          border: "1px solid #000",
+          borderRadius: "5px",
+          fontSize: "1.5rem"
+        }} type="text" placeholder="Gebe eine Stadt ein" />
+        <button 
+        style={{
+          padding: "1rem",
+          border: "1px solid #000",
+          borderRadius: "5px",
+          fontSize: "1.5rem"
+        }}
+        onClick={handleAddCity}>Wetter abfragen</button>
+        <br />
+        <ul>
+        {myCity.map(city => {
+        console.log("Temperatur vor Runden:", city.temp); // Diese Zeile hinzufügen
+        return (
+        <li key={city.id}>
+            {city.description} |
+            Temperatur: {Math.round(city.temp - 273.15)}°C |
+            Luftfeuchtigkeit: {city.humidity}%
+        </li>
+    );
+})}
+
+        </ul>
+        <br />
+        {/* 
+        <Button 
+        onClick={handleAddCity}
+        text={"City hinzufügen"} 
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        */}
+      </main>
+    </div>
+)
 }
